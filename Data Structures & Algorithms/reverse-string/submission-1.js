41b1c1ea951b149c045067c0396dc8ca08a06150
Sujit Samal <49195799+sujitsamaltechworks@users.noreply.github.com>
2026-07-19 13:18:06 +0530

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {character[]} s
+     * @return {void} Do not return anything, modify s in-place instead.
+     */
+    reverseString(s) {
+        let i=0, j=s.length - 1;
+        while(i<j) {
+            let temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+        return s;
+    }
+}
