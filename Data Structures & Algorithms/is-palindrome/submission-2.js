6497eeadcc4fa8277cabbf4075aeb481605de7e3
Sujit Samal <49195799+sujitsamaltechworks@users.noreply.github.com>
2026-07-20 11:58:43 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const cleaned = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+        let i = 0,
+            j = cleaned.length - 1;
+        while (i < j) {
+            if (cleaned[i] !== cleaned[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
